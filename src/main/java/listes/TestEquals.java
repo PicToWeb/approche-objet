88556d3742f8f0adb5c7ad9a36a9f55aeb8cdf9d
PicToWeb <picronantoine34@gmail.com>
2024-05-16 09:47:31 +0200

    Equals method

--- src/main/java/listes/TestEquals.java
+++ src/main/java/listes/TestEquals.java
@@ -5,19 +5,19 @@ public class TestEquals {
 	public static void main(String[] args) {
 		
 		Ville v1 = new Ville("Montpellier",34000);
-		Ville v2 = new Ville("Montpellier",34000);
+		Ville v2 = new Ville("Montpellierr",34000);
 		
 		if(v1.equals(v2)) {
-			System.err.println("yeah");
+			System.out.println("yeah");
 		}else {
-			System.out.println("ohh no");
+			System.err.println("ohh no");
 		}
 		
 		v1=v2;
 		if(v1 == v2) {
-			System.err.println("yeah 2");
+			System.out.println("yeah 2");
 		}else {
-			System.out.println("ohh no 2");
+			System.err.println("ohh no 2");
 		}
 
 	}
